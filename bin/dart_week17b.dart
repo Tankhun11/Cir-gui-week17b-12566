f9f2274750bcd867ef3c4f75ce29f15ec4fe0730
Thaenkhun <tankoon300950@gmail.com>
2023-09-08 09:37:45 +0700

    first app

---
+++ bin/dart_week17b.dart
@@ -0,0 +1,10 @@
+import 'package:dart_week17b/anonymouse.dart';
+import 'package:dart_week17b/parameter_and_return.dart';
+
+void main(){
+  prototype();
+}
+void prototype(){
+  setandanonymouse();
+  setname();
+}
